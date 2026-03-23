46c9c763451fe576e3b23c5036d44c5fb241f407
Adarsh Tiwari <tadarsh2006@gmail.com>
2026-03-23 18:16:35 +0530

    Data Visulaization

--- student-performance.py
+++ student-performance.py
@@ -33,3 +33,10 @@ custom_input = [[6, 80, 70]]
 predicted_score = model.predict(custom_input)
 print("\nPredicted Final Score:", predicted_score[0])
 
+# Step 7: Visualization
+
+plt.scatter(df['Study_Hours'], df['Final_Score'])
+plt.xlabel("Study Hours")
+plt.ylabel("Final Score")
+plt.title("Study Hours vs Final Score")
+plt.show()
