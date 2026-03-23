c0dde58e7683b5faa6fb0bf94359bd48b216ed9c
Adarsh Tiwari <tadarsh2006@gmail.com>
2026-03-23 18:07:12 +0530

    Reading csv

--- student-performance.py
+++ student-performance.py
@@ -3,3 +3,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv("customstudentdataset.csv")
